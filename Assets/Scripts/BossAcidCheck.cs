d8141e8219c640bc00a1d6f47e8ed728caac05b5
jakurll <jakurlng@gmail.com>
2023-04-12 00:36:32 -0500

    continued work on Bosspaint
    (NOT FINISHED)

--- Assets/Scripts/BossAcidCheck.cs
+++ Assets/Scripts/BossAcidCheck.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BossAcidCheck : MonoBehaviour
 {
     private CharacterController _controller;
+    [SerializeField] Color floorColor;
+    [SerializeField] Texture2D texture2D;
+    [SerializeField] Texture texture;
+    [SerializeField] Material material;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +25,24 @@ public class BossAcidCheck : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
-                var texture = GetComponent<Material>().GetTexture("_Mask");
-                var texture2D = (Texture2D)texture;
-                Color floorColor = texture2D.GetPixel((int)hit.point.x, (int)hit.point.y);
+                if (hit.transform.gameObject.layer == 10)
+                {
+                    material = hit.transform.gameObject.GetComponent<MeshRenderer>().sharedMaterial;
+                    texture = material.GetTexture("_Mask");
+                    texture2D = new Texture2D(texture.width, texture.height);
+                    StartCoroutine(CheckPixels());
+                    texture2D.Apply();
+                    floorColor = texture2D.GetPixel((int)hit.point.x, (int)hit.point.y);
 
-                
+                    Debug.Log(floorColor);
+                }
             }
         }
+    }
+
+    IEnumerator CheckPixels()
+    {
+        yield return new WaitForEndOfFrame();
+        texture2D.ReadPixels(new Rect(0f, 0f, texture.width, texture.height), 0, 0);
     }
 }
