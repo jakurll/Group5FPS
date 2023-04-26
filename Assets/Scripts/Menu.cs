574a5f39f6120c73646ad6d3a92fd9bf0b78ccde
lande119 <lande119@stumail.jccc.edu>
2023-04-26 11:34:15 -0500

    textures and update menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,18 +7,12 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit Game");
         Application.Quit();
-    }
-
-    public void ClickToHome()
-    {
-        Debug.Log("Goes to Menu");
-        SceneManager.LoadScene(0);
     }
 }
