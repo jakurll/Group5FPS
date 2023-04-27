565aa7afc1e0d8a718a2ea606a407b776999f7b5
lande119 <lande119@stumail.jccc.edu>
2023-04-27 00:50:14 -0500

    key spawn

--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    void FindObjects()
     {
-        PlayerInventory inventory = other.GetComponent<PlayerInventory>();
-        if(inventory != null )
-        {
-            if(inventory.hasKey == true)
-            {
+        GameObject[] objects;
+        objects = GameObject.FindGameObjectsWithTag("Enemy");
 
+        //If there are no more enemies, spawn key pickup
+        if (objects.Length == 0)
+        {
+            void OnTriggerEnter(Collider other)
+            {
+                //Check if player has the key
+                PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+                if (inventory != null)
+                {
+                    if (inventory.hasKey == true)
+                    {
+
+                    }
+                    inventory.hasKey = true;
+                    Destroy(this.gameObject);
+                }
             }
-            inventory.hasKey = true;
-            Destroy(this.gameObject);
+        }
+        else
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(true);
+            }
         }
     }
 }
