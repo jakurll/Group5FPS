f6841d88591b99e0df5190fba70fa51d73a4fc2a
rootCaptain1 <lsterli2@stumail.jccc.edu>
2023-04-28 22:43:36 -0500

    Key Script Update

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public bool hasKey = false;
+
+  public void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Key"))
+    {
+      hasKey = true;
+    }
+  }
 }
